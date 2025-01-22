b6a4e3769df2809b3a2fe3cb49b66b920c7ff2d9
DHANA VASANTH <117557948+dhanavasanth@users.noreply.github.com>
2025-01-22 09:57:09 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("welcome to my app...!")
+
+name = st.text_input('enter your name..!')
+
+if name is not None and st.button("click"):
+    st.write(name)
+else:
+    st.write("please enter a valid name...!")
